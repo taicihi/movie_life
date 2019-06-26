bcbfbacb98f7286168f2246aab02d3389f310163
soccer1dkomau <soccer1dkomau@gmail.com>
2019-06-26 11:07:46 +0000

    second commit

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
   root 'pages#index'
   resources :users
+  
+  get    '/login',   to: 'sessions#new'
+  post   '/login',   to: 'sessions#create'
+  delete '/logout',  to: 'sessions#destroy'
+  
+  get 'homes/index'
+  get 'homes/film'
+  get 'homes/theater'
+  get 'homes/ask'
 end
