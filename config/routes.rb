dd8b836eba1f3e0fc403ff2d47bc9f064137e5ee
soccer1dkomau <soccer1dkomau@gmail.com>
2019-07-02 13:19:16 +0000

    ask_form

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,9 @@ Rails.application.routes.draw do
   get 'homes/film'
   get 'homes/theater'
   get 'homes/ask'
+  
+  get 'inquiry' => 'inquiry#index'              # 入力画面
+  post 'inquiry/confirm' => 'inquiry#confirm'   # 確認画面
+  post 'inquiry/thanks' => 'inquiry#thanks'     # 送信完了画面
+  
 end
